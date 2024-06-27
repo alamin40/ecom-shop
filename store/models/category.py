79957d63433d9d3de38d36039a7d7fcc3b4573fe
alamin40 <hossain.78295@gmail.com>
2024-06-27 03:12:41 +0200

    catergory model class added

--- store/models/category.py
+++ store/models/category.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name= models.CharField(max_length=50)
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
